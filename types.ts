e1e8c9b5fd56856bed29dfc21ea55cbcfb1549ce
Amr Gad <merodiro@gmail.com>
2021-01-28 01:54:09 +0200

    New baby Blitz app!

---
+++ types.ts
@@ -0,0 +1,16 @@
+import { DefaultCtx, SessionContext } from "blitz"
+import { simpleRolesIsAuthorized } from "@blitzjs/server"
+import { User } from "db"
+
+declare module "blitz" {
+  export interface Ctx extends DefaultCtx {
+    session: SessionContext
+  }
+  export interface Session {
+    isAuthorized: typeof simpleRolesIsAuthorized
+    PublicData: {
+      userId: User["id"]
+      roles: string[]
+    }
+  }
+}

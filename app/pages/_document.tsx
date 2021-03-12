b6c077030446d0d9b367e5e99b9e962b761b1784
Amr Gad <merodiro@gmail.com>
2021-03-13 01:11:23 +0200

    chore: use @twind/next

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,43 +1,8 @@
-import { Document, Html, DocumentHead, Main, BlitzScript, DocumentContext } from 'blitz'
-import { createElement } from 'react'
-import { setup } from 'twind'
-import { virtualSheet, getStyleTagProperties, shim } from 'twind/shim/server'
+import { Document, Html, DocumentHead, Main, BlitzScript } from 'blitz'
+import withTwindDocument from '@twind/next/shim/document'
 import twindConfig from 'app/core/twind.config'
 
-const sheet = virtualSheet()
-
-setup({ sheet, ...twindConfig })
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    sheet.reset()
-
-    const originalRenderPage = ctx.renderPage
-
-    ctx.renderPage = async (options) => {
-      let { html, head } = await originalRenderPage(options)
-
-      html = shim(html)
-
-      const { id, textContent } = getStyleTagProperties(sheet)
-
-      return {
-        html,
-        head: [
-          ...(head ?? []),
-          createElement('style', {
-            id: '__next' + id,
-            key: id,
-            dangerouslySetInnerHTML: {
-              __html: textContent,
-            },
-          }),
-        ],
-      }
-    }
-
-    return Document.getInitialProps(ctx)
-  }
-
   render() {
     return (
       <Html lang="en">
@@ -51,4 +16,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument
+export default withTwindDocument(twindConfig, MyDocument)

7ff01ba0bedc4da092703660ce662c438c07cbfd
Amr Gad <merodiro@gmail.com>
2021-10-19 21:02:03 +0200

    chore: use planetscale for db

---
+++ db/migrations/20211019185710_init/migration.sql
@@ -0,0 +1,57 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `avatar` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `hashedPassword` VARCHAR(191) NULL,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `expiresAt` DATETIME(3) NULL,
+    `handle` VARCHAR(191) NOT NULL,
+    `hashedSessionToken` VARCHAR(191) NULL,
+    `antiCSRFToken` VARCHAR(191) NULL,
+    `publicData` VARCHAR(191) NULL,
+    `privateData` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Session_handle_key`(`handle`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Token` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `hashedToken` VARCHAR(191) NOT NULL,
+    `type` ENUM('RESET_PASSWORD') NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `sentTo` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Token_hashedToken_type_key`(`hashedToken`, `type`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `body` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
